64dc3dea1ee60fafd3a424c19138046484f726fa
Camilo <asfaltorodas@gmail.com>
2023-07-01 19:57:57 -0300

    att project api consumed

---
+++ dependencies/consume_api.py
@@ -0,0 +1,20 @@
+import requests
+
+
+def get_api_data():
+    url: str = f'https://api.publicapis.org/entries'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        try:
+            data: dict = dict(response.json())
+            print(f'JSON pedido: {data["count"]}')
+            return data
+        except ValueError:
+            print("A resposta não contém um JSON válido")
+    else:
+        print(f"A solicitação retornou um erro: {response.status_code}")
+        print(response.text)
+
+
+get_api_data()
--- site_api_list.py
+++ site_api_list.py
@@ -1,14 +1,28 @@
 from flask import Flask
 from flask import render_template as page
-
-# Instance of Site
+from dependencies import consume_api
+# Start Instance
 app = Flask(__name__)
 
 
-# route
+# routes
 @app.route(r'/')
 def home_page():
-    return page("homepage.html")
+    datas: dict = consume_api.get_api_data()
+    count: int = datas['count']
+    list_apis: list = datas['entries']
+    names_api = []
+    links_api = []
+    descriptions_api = []
+    for api in list_apis:
+        names_api.append(api['API'])
+        links_api.append(api['Link'])
+        descriptions_api.append(api['Description'])
+    return page("homepage.html",
+                count=count,
+                names_api=names_api,
+                links_api=links_api,
+                descriptions_api=descriptions_api)
 
 
 @app.route(r'/users/<user_name>')
